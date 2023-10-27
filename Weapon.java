d9430f735277592b69e2ca6dccf665e4dff5c846
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-27 09:54:23 +0200

    task1

---
+++ Weapon.java
@@ -0,0 +1,33 @@
+abstract class Weapon {
+
+    protected String name;
+    protected int apcost;
+    protected int damage;
+    protected boolean melee;
+
+    protected Weapon(String name, int apcost, int damage, boolean melee) {
+        this.name = name;
+        this.apcost = apcost;
+        this.damage = damage;
+        this.melee = melee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getApcost() {
+        return apcost;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public boolean getMelee() {
+        return melee;
+    }
+
+    public abstract void attack();
+
+}
